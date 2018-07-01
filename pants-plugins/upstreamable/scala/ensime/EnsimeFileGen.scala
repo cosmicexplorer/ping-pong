f79df09cc722d256651c20ece2969ed3b8df0762
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-07-01 05:34:25 -0700

    just actually generate everything in the ensime config except projects

--- pants-plugins/upstreamable/scala/ensime/EnsimeFileGen.scala
+++ pants-plugins/upstreamable/scala/ensime/EnsimeFileGen.scala
@@ -1,16 +1,33 @@
 package pingpong.ensime
 
-import pingpong.ensime.PantsExport
 import pingpong.ensime.PantsExportProtocol._
 
 import org.ensime.api._
 import spray.json._
 
+import java.io.File
+
 object EnsimeFileGen extends App {
   val allStdin = scala.io.Source.stdin.mkString
   val pantsExportParsed = allStdin.parseJson.convertTo[PantsExport]
 
-  println(s"hello, world:\n${pantsExportParsed.toJson.prettyPrint}")
+  val Array(buildRoot, scalaVersion, ensimeCacheDir) = args
+
+  val defaultJvmPlatform = pantsExportParsed.jvmPlatforms.defaultPlatform
+  val javaHome = pantsExportParsed.preferredJvmDistributions(defaultJvmPlatform).strict
+
+  val ensimeConfig = EnsimeConfig(
+    name = "???",
+    rootDir = RawFile(new File(buildRoot).toPath),
+    cacheDir = RawFile(new File(ensimeCacheDir).toPath),
+    scalaVersion = scalaVersion,
+    javaHome = RawFile(new File(javaHome).toPath),
+    javaSources = List(),
+    projects = List(),
+    compilerArgs = List(),
+  )
+
+  println(s"hello, world:\n${ensimeConfig}")
 
   println("hey!")
 }
--- pants-plugins/upstreamable/tasks/ensime_gen.py
+++ pants-plugins/upstreamable/tasks/ensime_gen.py
@@ -4,6 +4,8 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import json
 import os
 
+from pants.base.build_environment import get_buildroot
+from pants.backend.jvm.subsystems.scala_platform import ScalaPlatform
 from pants.backend.jvm.tasks.jvm_tool_task_mixin import JvmToolTaskMixin
 from pants.backend.project_info.tasks.export import ExportTask
 from pants.build_graph.address import Address
@@ -14,6 +16,7 @@ from pants.java.util import execute_java
 from pants.option.custom_types import target_option
 from pants.util.collections import assert_single_element
 from pants.util.contextutil import temporary_dir
+from pants.util.dirutil import safe_mkdir
 from pants.util.memo import memoized_property
 from pants.util.objects import SubclassesOf
 
@@ -23,13 +26,26 @@ from upstreamable.tasks.bootstrap_ensime_gen import EnsimeGenJar
 class EnsimeGen(ExportTask, JvmToolTaskMixin):
 
   @classmethod
+  def register_options(cls, register):
+    super(EnsimeGen, cls).register_options(register)
+
+    register('--reported-scala-version', type=str, default=None,
+             help='Scala version to report to ensime. Defaults to the scala platform version.')
+
+  @classmethod
   def prepare(cls, options, round_manager):
     round_manager.require_data(EnsimeGenJar)
     cls.prepare_tools(round_manager)
 
   @classmethod
   def subsystem_dependencies(cls):
-    return super(EnsimeGen, cls).subsystem_dependencies() + (DistributionLocator,)
+    return super(EnsimeGen, cls).subsystem_dependencies() + (DistributionLocator, ScalaPlatform,)
+
+  def _make_ensime_cache_dir(self):
+    bootstrap_dir = os.path.expanduser(self.get_options().pants_bootstrapdir)
+    cache_dir = os.path.join(bootstrap_dir, 'ensime')
+    safe_mkdir(cache_dir)
+    return cache_dir
 
   def execute(self):
 
@@ -49,9 +65,21 @@ class EnsimeGen(ExportTask, JvmToolTaskMixin):
         ensime_gen_jar = self.context.products.get_data(EnsimeGenJar)
         ensime_gen_classpath = [ensime_gen_jar.tool_jar_path]
 
+        # TODO: use JvmPlatform for jvm options!
+        reported_scala_version = self.get_options().reported_scala_version
+        if not reported_scala_version:
+          reported_scala_version = ScalaPlatform.global_instance().version
+
+        argv = [
+          get_buildroot(),
+          reported_scala_version,
+          self._make_ensime_cache_dir(),
+        ]
+
         with open(export_outfile, 'rb') as inf:
           execute_java(ensime_gen_classpath,
                        'pingpong.ensime.EnsimeFileGen',
+                       args=argv,
                        workunit_name='ensime-gen',
                        workunit_labels=[WorkUnitLabel.TOOL],
                        distribution=DistributionLocator.cached(),
